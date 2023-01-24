8a026d25ad392cab2e03e91b9ddd234450f52e14
HANA-BOOK\smile <smilecana@gmail.com>
2023-01-24 06:17:17 -0800

    updated

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,8 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('/*',function(req, res){
   res.sendFile(path.join(__dirname, '../frontend/build','index.html'));
 })
+console.log('__dirname', __dirname);
+console.log('filename', __filename);
 
 app.get("*", function (req, res) {
   console.log("404 - ");
