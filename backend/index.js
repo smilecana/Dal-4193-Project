49b962f922d6b5b2639b8d4313d84c9a3176c06a
HANA-BOOK\smile <smilecana@gmail.com>
2023-01-28 14:28:01 -0800

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,12 +29,11 @@ app.use("/api/feed", feedRoute)
 app.use(ordersRoute);
 
 // Serve Static assests if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, './frontend/build')));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build'));
+});
+
 app.get("*", function (req, res) {
   console.log("404 - ");
   res.send("404");
